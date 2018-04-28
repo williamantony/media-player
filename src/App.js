e4651c5b5478c0e928c3cb93fcf24c2c4a9788b2
William Antony <william@pathadan.com>
2018-04-27 23:48:31 -0500

    Added Player Component to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Player from './components/Player/Player';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      
+        <Player />
       </div>
     );
   }
